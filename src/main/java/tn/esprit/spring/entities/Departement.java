aed12ba41cc5c0a6c90bf0cdcd42bc91c1a18c8d
oussamaakrout <akrout.oussama@esprit.tn>
2021-11-03 11:50:33 +0100

    oussamaakrout

---
+++ src/main/java/tn/esprit/spring/entities/Departement.java
@@ -0,0 +1,117 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+public class Departement implements Serializable {
+
+	private static final long serialVersionUID = -357738161698377833L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	//NOSONAR@JsonManagedReference 
+	//NOSONAR	@JsonIgnore
+	//NOSONAR	@ManyToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	//NOSONAR	private List<Employe> employes;
+	
+	@OneToMany(mappedBy="departement")
+	private List<Mission> missions;
+	
+	@ManyToOne
+	private Entreprise entreprise;
+
+	public Departement() {
+		super();
+	}
+	
+	public Departement(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//NOSONAR	public List<Employe> getEmployes() {
+	//NOSONAR		return employes;
+	//NOSONAR	}
+
+	//NOSONAR	public void setEmployes(List<Employe> employes) {
+	//NOSONAR		this.employes = employes;
+
+	public List<Mission> getMissions() {
+		return missions;
+	}
+
+	public void setMissions(List<Mission> missions) {
+		this.missions = missions;
+	}
+
+	public Entreprise getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(Entreprise entreprise) {
+		this.entreprise = entreprise;
+	}
+
+	public Departement(String name, Entreprise entreprise) {
+		super();
+		this.name = name;
+		this.entreprise = entreprise;
+	}
+
+	public Departement(int id, String name, Entreprise entreprise) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.entreprise = entreprise;
+	}
+
+	public Departement(int id, String name, List<Mission> missions, Entreprise entreprise) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.missions = missions;
+		this.entreprise = entreprise;
+	}
+
+	public Departement(int id) {
+		super();
+		this.id = id;
+	}
+
+	
+	
+
+}
---
+++ src/test/java/tn/esprit/spring/service/DepartementServiceTest.java
@@ -0,0 +1,75 @@
+package tn.esprit.spring.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import org.junit.Assert;
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.services.IDepartementService;
+import java.text.ParseException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepartementServiceTest {
+	
+	@Autowired
+	IDepartementService ds ;
+	
+	@Test
+	public void testModifyDepartement() throws ParseException   {
+		
+		Departement d = new Departement(29,"change",null);
+		Departement departementModified = ds.updateDepartement(d) ;
+		Assert.assertEquals(d.getName(), departementModified.getName());
+	}
+
+	@Test
+	public void testDeleteDepartement() {
+		ds.remove("28");
+		Assert.assertNull(ds.retrieveDepartement("28"));
+		
+		
+	}
+
+	@Test
+	public void testRetrieveAllEmploye() {
+		List<Departement> listDepartements = ds.retrieveAllDepartements();
+		Assert.assertEquals(1, listDepartements.size());
+	}
+	
+	@Test
+	public void testAddDepartement() throws ParseException {
+		
+		
+		Departement d = new Departement("new", null );
+		Departement departementadded = ds.addDepartement(d) ;
+		Assert.assertEquals(d.getName(), departementadded.getName());
+	}
+	@Test
+	public void testRetrieveDepartement() {
+		Departement d = ds.retrieveDepartement("29");
+		Assert.assertEquals(29, d.getId());
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
